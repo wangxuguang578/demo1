129ee7c40d12801a158a474cb1baafeec2fdb1f1
xuguang <xuguang.wang@zhiyoubao.com>
2019-07-30 10:07:03 +0800

    assert

--- src/main/java/com/wxg/demo/web/JenkinsController.java
+++ src/main/java/com/wxg/demo/web/JenkinsController.java
@@ -5,11 +5,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+
 @RestController
 @RequestMapping("jenkins")
 public class JenkinsController {
 
-    @SuppressWarnings("AlibabaLowerCamelCaseVariableNaming")
     @Autowired
     private JenkinsServiceImpl jenkinsService;
     @RequestMapping("test1")
